44a56dab18ffee34258682404a99121e42924d41
vagrant <vagrant@homestead>
2018-12-22 12:07:11 +0000

    Basic user profile editing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // 在 Laravel 中，时间戳 created_at 和 updated_at 作为模型属性被调用时，都会自动转换为 Carbon 对象
+        // diffForHumans() 是 Carbon 对象提供的方法，默认情况是英文的，如果要使用中文时间提示，则需要对 Carbon 进行本地化设置
+        \Carbon\Carbon::setLocale('zh');
     }
 
     /**
---
+++ database/migrations/2018_12_22_155722_add_avatar_and_introduction_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAvatarAndIntroductionToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // table() 表示向表中加字段；create()表示创建表
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('avatar')->nullable();
+            $table->string('introduction')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // table()表示删除表中的字段
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropcColumn('avatar');
+            $table->dropcColumn('introduction');
+        });
+    }
+}

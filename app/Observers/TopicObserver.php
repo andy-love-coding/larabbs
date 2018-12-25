788b81a309cbeda042ef361e036365b99ddf2729
vagrant <vagrant@homestead>
2018-12-25 15:09:15 +0000

    Prevent XSS injection

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -19,8 +19,12 @@ class TopicObserver
         //
     }
 
+    // 数据入库前：进行过滤(防止xss攻击，过滤掉危险代码)、属性赋值
     public function saving(Topic $topic)
     {
+        // clean() 是 HTMLPurifier 插件的方法，'user_topic_body' 是在 config/purifier.php 中配置的
+        $topic->body = clean($topic->body, 'user_topic_body'); 
+        // make_excerpt() 是在 bootsrap/helper.php 中自定义的辅助函数
         $topic->excerpt = make_excerpt($topic->body);
     }
 }

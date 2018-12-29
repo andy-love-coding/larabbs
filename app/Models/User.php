783aa3c51e96b5cd331cca5ac6920e4c24ea5762
vagrant <vagrant@homestead>
2018-12-29 12:31:42 +0000

    Integrated administrator

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,11 +28,6 @@ class User extends Authenticatable
         $this->laravelNotify($instance);
     }
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password','introduction','avatar',
     ];
